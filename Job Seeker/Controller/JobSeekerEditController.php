f716c6a119c5005515d267b1c1b86fbdc1688627
MST SHIFA LILA <shifalaila311@gmail.com>
2026-01-15 19:11:18 +0600

    jobseeker feature: UI

--- Job Seeker/Controller/JobSeekerEditController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'models/JobSeekerModel.php';
-
-function jobSeeker_editProfileAction() {
-    $userId = $_SESSION['user_id'];
-    return jobSeeker_getProfileByUserId($userId);
-}
-?>
